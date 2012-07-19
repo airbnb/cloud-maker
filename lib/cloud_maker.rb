7228642455a6653bde30feea94970a7fe8cf5b04
Nathan Baxter <nathan.baxter@airbnb.com>
2012-07-19 10:07:55 -0700

    Refactored the config file design so we don't need special treatment of environment variables, and separated responsibility for AWS credentials out of the config completely.

--- lib/cloud_maker.rb
+++ lib/cloud_maker.rb
@@ -2,18 +2,27 @@ require './lib/cloud_maker_config'
 require 'right_aws'
 
 class CloudMaker
-  attr_accessor :config
-  def initialize(cloud_maker_config)
+  attr_accessor :config, :aws_secret_access_key, :aws_access_key_id
+
+  def initialize(cloud_maker_config, options)
     self.config = cloud_maker_config
+    self.aws_access_key_id = options[:aws_access_key_id]
+    self.aws_secret_access_key = options[:aws_secret_access_key]
   end
 
   def launch
-    ec2 = RightAws::Ec2.new(self.config['AWS_ACCESS_KEY_ID'], self.config['AWS_SECRET_ACCESS_KEY'])
+    ec2 = RightAws::Ec2.new(self.aws_access_key_id, self.aws_secret_access_key)
     binding.pry
   end
 
+  def valid?
+    !self.config.nil? &&
+    !self.aws_secret_access_key.nil? && !self.aws_secret_access_key.empty? &&
+    !self.aws_access_key_id.nil? && !self.aws_access_key_id.empty?
+  end
+
   class << self
-    def from_yaml(instance_config_yaml)
+    def from_yaml(instance_config_yaml, options)
       begin
         full_path = File.expand_path(instance_config_yaml)
         cloud_yaml = File.open(full_path, "r") #Right_AWS will base64 encode this for us
@@ -27,7 +36,7 @@ class CloudMaker
         raise "ERROR: The CloudMaker config contained invalid YAML syntax"
       end
 
-      self.new(CloudMakerConfig.new(cloud_config))
+      self.new(CloudMakerConfig.new(cloud_config), options)
     end
   end
 end
--- lib/cloud_maker_config.rb
+++ lib/cloud_maker_config.rb
@@ -1,6 +1,14 @@
 class CloudMakerConfig
   attr_accessor :options, :cloud_config
 
+  # If you don't specify a property associated with a key in the cloud_maker config file
+  # we will use these properties to fill in the blanks
+  DEFAULT_KEY_PROPERTIES = {
+    :environment => true,
+    :required => false,
+    :value => nil
+  }
+
   def initialize(cloud_config)
     cloud_config = cloud_config.dup
     self.options = extract_cloudmaker_config!(cloud_config)
@@ -20,6 +28,17 @@ class CloudMakerConfig
   end
 
   def extract_cloudmaker_config!(config)
-    config.delete('cloud_maker')
+    cloud_maker_config = config.delete('cloud_maker')
+    cloud_maker_config.keys.each do |key|
+      #if key is set to anything but a hash then we treat it as the value property
+      if !cloud_maker_config[key].kind_of?(Hash)
+        cloud_maker_config[key] = {
+          :value => cloud_maker_config[key]
+        }
+      end
+
+      cloud_maker_config[key] = DEFAULT_KEY_PROPERTIES.merge(cloud_maker_config[key])
+    end
+    cloud_maker_config
   end
 end
