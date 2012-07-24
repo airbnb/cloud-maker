62fbca9f0953ba1bce159058610cba5baad73bde
Nathan Baxter <nathan.baxter@airbnb.com>
2012-07-23 17:44:52 -0700

    More TomDoc documentation and some minor refactoring of the archiver to make it friendlier for loading archives.

--- lib/cloud_maker/ec2.rb
+++ lib/cloud_maker/ec2.rb
@@ -2,21 +2,42 @@ require 'right_aws'
 
 module CloudMaker
   class Ec2
-    attr_accessor :config, :aws_secret_access_key, :aws_access_key_id
+    # Public: Gets/Sets the AWS access key.
+    attr_accessor :aws_secret_access_key
+    # Public: Gets/Sets the AWS secret.
+    attr_accessor :aws_access_key_id
+    # Public: Gets/Sets the CloudMaker::Config
+    attr_accessor :config
 
+    # Public: Creates a new Ec2 instance
+    #
+    # cloud_maker_config - A CloudMaker::Config object describing the instance
+    #                      to be managed.
+    # options            - S3 configuration options
+    #                      :aws_access_key_id     - (required) The AWS access key
+    #                      :aws_secret_access_key - (required) The AWS secret
+    #
+    # Returns a new CloudMaker::Ec2 instance
+    # Raises RuntimeError if any of the required options are not specified
     def initialize(cloud_maker_config, options)
+      required_keys = [:aws_access_key_id, :aws_secret_access_key]
+      unless (required_keys - options.keys).empty?
+        raise RuntimeError.new("Instantiated #{self.class} without required attributes: #{required_keys - options.keys}.")
+      end
+
       self.config = cloud_maker_config
       self.aws_access_key_id = options[:aws_access_key_id]
       self.aws_secret_access_key = options[:aws_secret_access_key]
     end
 
+    # Public: Launches a new EC2 instance, associates any specified elastic IPS
+    # with it, adds any specified tags, and archives the launch details to S3.
+    #
+    # Returns a RightAws supplied Hash describing the launched instance.
     def launch
       ec2 = RightAws::Ec2.new(self.aws_access_key_id, self.aws_secret_access_key)
 
       user_data = self.config.to_user_data
-
-      output = File.open('generated-cloud-config.yaml', 'w')
-      output.puts user_data
 
       instance = ec2.launch_instances(config['ami'],
         :group_names => config['security_group'],
@@ -41,19 +62,20 @@ module CloudMaker
         end
 
         ec2.associate_address(instance_id, :public_ip => self.config["elastic_ip"])
+
+        instance = ec2.describe_instances([instance_id]).first # So we get the correct IP address
       end
 
 
-      archiver = S3Archiver.new(self.config, instance, :aws_access_key_id => self.aws_access_key_id, :aws_secret_access_key => self.aws_secret_access_key)
-      archiver.store_archive
+      archiver = S3Archiver.new(
+        :instance_id => instance_id,
+        :aws_access_key_id => self.aws_access_key_id,
+        :aws_secret_access_key => self.aws_secret_access_key,
+        :bucket_name => self.config["s3_archive_bucket"]
+      )
+      archiver.store_archive(self.config, instance)
 
       instance
-    end
-
-    def valid?
-      !self.config.nil? &&
-      !self.aws_secret_access_key.nil? && !self.aws_secret_access_key.empty? &&
-      !self.aws_access_key_id.nil? && !self.aws_access_key_id.empty?
     end
   end
 end
--- lib/cloud_maker/s3_archiver.rb
+++ lib/cloud_maker/s3_archiver.rb
@@ -2,43 +2,66 @@ require 'right_aws'
 
 module CloudMaker
   class S3Archiver
-    attr_accessor :config, :aws_secret_access_key, :aws_access_key_id, :instance, :bucket
 
+    # Public: Gets/Sets the AWS access key.
+    attr_accessor :aws_secret_access_key
+    # Public: Gets/Sets the AWS secret.
+    attr_accessor :aws_access_key_id
+    # Public: Gets/Sets the EC2 instance ID string.
+    attr_accessor :instance_id
+    # Internal: Gets/Sets the RightAws::S3::Bucket used for storing/loading archives.
+    attr_accessor :bucket
+
+    # Public: All archive keys will be prefixed with KEY_PREFIX/
     KEY_PREFIX = "cloud-maker"
 
-    def initialize(cloud_maker_config, instance, options)
-      self.config = cloud_maker_config
-      self.instance = instance
+    # Public: Creates a new S3 Archiver instance
+    #
+    # options - S3 configuration options
+    #           :aws_access_key_id     - (required) The AWS access key
+    #           :aws_secret_access_key - (required) The AWS secret
+    #           :bucket_name           - (required) The bucket for the archiver to access
+    #           :instance_id           - (required) The AWS instance ID the archive describes
+    #
+    # Returns a new CloudMaker::S3Archiver instance
+    # Raises RuntimeError if any of the required options are not specified
+    def initialize(options)
+      required_keys = [:aws_access_key_id, :aws_secret_access_key, :instance_id, :bucket_name]
+      unless (required_keys - options.keys).empty?
+        raise RuntimeError.new("Instantiated #{self.class} without required attributes: #{required_keys - options.keys}.")
+      end
+
+      self.instance_id = options[:instance_id]
       self.aws_access_key_id = options[:aws_access_key_id]
       self.aws_secret_access_key = options[:aws_secret_access_key]
+
       s3 = RightAws::S3.new(self.aws_access_key_id, self.aws_secret_access_key)
-      self.bucket = s3.bucket(self.bucket_name)
+      self.bucket = s3.bucket(options[:bucket_name])
     end
 
-    def store_archive
-      archive = self.config.to_archive
+    # Public: Generates an archive with all information relevant to an instance
+    # launch and stores it to S3.
+    #
+    # Returns nothing.
+    def store_archive(cloud_maker_config)
+      archive = cloud_maker_config.to_archive
       self.bucket.put(self.key, archive)
     end
 
+    # Public: Retrieves a previously created archive from S3
+    #
+    # Returns the content of the archive.
+    def load_archive
+      self.bucket.get(self.key)
+    end
+
+    # Public: Returns the key that the archive will be stored under
     def key
       if self.instance_id
         [KEY_PREFIX, self.instance_id].join('/')
       else
         raise RuntimeError.new("Attempted to generate a key name without an instance id.")
       end
-    end
-
-    def instance_id
-      self.instance[:aws_instance_id]
-    end
-
-    def bucket_name
-      raise RuntimeError.new("Attempted to access S3 bucket name, which hasn't been defined.") unless self.config["s3_archive_bucket"]
-      self.config["s3_archive_bucket"]
-    end
-
-    def load_archive
-      self.bucket.get(self.key)
     end
   end
 end
